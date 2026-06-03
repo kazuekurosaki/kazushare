ef4047b6ccf983a27a612c579f4ba38856b6cbd1
Kazue Kurosaki <137025030+kazuekurosaki@users.noreply.github.com>
2026-06-04 01:30:19 +0700

    Create App.kt

---
+++ app/src/main/java/com/example/filestorage/App.kt
@@ -0,0 +1,9 @@
+// name=app/src/main/java/com/example/filestorage/App.kt
+package com.example.filestorage
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+// Application class needed for Hilt
+@HiltAndroidApp
+class App : Application()
